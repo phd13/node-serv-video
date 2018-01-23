1ce5df449c1eaf4d2efbbe7407b3b8bd82221d48
Philipp <phd13@yandex.ru>
2018-01-23 23:25:50 +0400

    saving theme data to localStorage

--- public/main.js
+++ public/main.js
@@ -2,21 +2,48 @@ const api = 'http://localhost:8080/api';
 const input = document.getElementById('link');
 const form = document.querySelector('form');
 const video = document.getElementById('video');
+const stylesheet = document.getElementById('stylesheet');
+const themeButton = document.querySelector('.themeChange');
+
+window.onload = function () {
+	let theme = localStorage.getItem('theme');
+	if (theme) {
+		stylesheet.href = theme;
+	}
+};
+
+themeButton.onclick = function () {
+	let currentTheme = stylesheet.getAttribute('href');
+	switch (currentTheme) {
+		case 'dark.css':
+			stylesheet.href = 'light.css';
+			localStorage.setItem('theme', 'light.css');
+			break;
+
+		case 'light.css':
+			stylesheet.href = 'dark.css';
+			localStorage.setItem('theme', 'dark.css');
+			break;
+
+		default:
+			break;
+	}
+};
 
 let link = '';
-input.onchange = function(event) {
-	let { value } = event.target;
+input.onchange = function (event) {
+	let {value} = event.target;
 	link = value;
 };
 
-form.onsubmit = function(e) {
+form.onsubmit = function (e) {
 	e.preventDefault();
 	const request = new Request(`${api}/link`, {
 		method: 'POST',
 		headers: {
 			"Content-Type": "application/json"
 		},
-		body: JSON.stringify({ link })
+		body: JSON.stringify({link})
 	});
 
 	fetch(request)
@@ -27,7 +54,7 @@ form.onsubmit = function(e) {
 				return res.json();
 			}
 		})
-		.then(({ link, from }) => {
+		.then(({link, from}) => {
 			console.log(from);
 			video.src = link;
 			video.play();
